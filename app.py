c203811cf9545a3fd42f122214c9a7d493b735e6
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-04-20 20:30:18 +0200

    Removed the unused ropod management functionalities

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from __future__ import print_function
 import ast
 from flask import Flask, jsonify, render_template, request, session
 
-from constants import LocalDbConstants, DbConstants, VariableConstants
-from db import DbConnection, DbQueries
-from local_db import RopodAdminQueries
-
 import zmq
 import json
 import uuid
@@ -453,67 +449,6 @@ def read_ropod_status():
         message = 'Status could not be retrieved'
         return jsonify(ropod_status=None, message=message)
 ##########################################################
-
-@app.route('/get_hospital_ropod_ids', methods=['GET', 'POST'])
-def get_hospital_ropod_ids():
-    hospital = request.args.get('hospital', '', type=str)
-    ids = RopodAdminQueries.get_hospital_ropod_ids(local_db_connection, hospital)
-    return jsonify(ids=ids)
-
-@app.route('/manage_ropods')
-def manage_ropods():
-    hospitals, ids, ip_addresses = RopodAdminQueries.get_all_ropods(local_db_connection)
-    return render_template('manage_ropods.html', hospitals=hospitals,
-                           ids=ids, ip_addresses=ip_addresses)
-
-@app.route('/add_ropod')
-def add_ropod():
-    hospital_names = RopodAdminQueries.get_hospital_names(local_db_connection)
-    return render_template('add_ropod.html', hospitals=hospital_names)
-
-@app.route('/add_new_ropod', methods=['POST'])
-def add_new_ropod():
-    data = request.get_json(force=True)
-    hospital = data['hospital']
-    ropod_id = data['ropod_id']
-    ip_address = data['ip_address']
-    RopodAdminQueries.add_new_ropod(local_db_connection, hospital, ropod_id, ip_address)
-    return jsonify(success=True)
-
-@app.route('/edit_ropod')
-def edit_ropod():
-    hospital_names = RopodAdminQueries.get_hospital_names(local_db_connection)
-    original_hospital = request.args.get('hospital', '', type=str)
-    original_id = request.args.get('ropod_id', '', type=str)
-    original_ip_address = request.args.get('ip_address', '', type=str)
-    return render_template('edit_ropod.html',
-                           hospitals=hospital_names,
-                           original_hospital=original_hospital,
-                           original_id=original_id,
-                           original_ip_address=original_ip_address)
-
-@app.route('/update_existing_ropod', methods=['POST'])
-def update_existing_ropod():
-    data = request.get_json(force=True)
-    old_hospital = data['old_hospital']
-    old_id = data['old_id']
-    old_ip_address = data['old_ip_address']
-    new_hospital = data['new_hospital']
-    new_id = data['new_id']
-    new_ip_address = data['new_ip_address']
-    RopodAdminQueries.update_existing_ropod(local_db_connection,
-                                            old_hospital, old_id, old_ip_address,
-                                            new_hospital, new_id, new_ip_address)
-    return jsonify(success=True)
-
-@app.route('/delete_ropod', methods=['POST'])
-def delete_ropod():
-    data = request.get_json(force=True)
-    hospital = data['hospital']
-    ropod_id = data['ropod_id']
-    ip_address = data['ip_address']
-    RopodAdminQueries.delete_ropod(local_db_connection, hospital, ropod_id, ip_address)
-    return jsonify(success=True)
 
 if __name__ == '__main__':
     try:
