6feca209b9ac3b5cc76a994d3070a02d1e3d4c5d
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-10-30 09:09:59 +0100

    [experiments] The robot IDs are now read from a DB; the experiment trigger functionality is operational again

--- remote_monitoring/blueprints/experiments/experiments.py
+++ remote_monitoring/blueprints/experiments/experiments.py
@@ -1,61 +1,45 @@
 from __future__ import print_function
-from flask import Blueprint, jsonify, render_template, request, session
-
-import ast
 import json
 import uuid
 
-from remote_monitoring.common import msg_data, communicate_zmq
+from flask import Blueprint, jsonify, render_template, request, session
+from remote_monitoring.common import msg_data, communicate_zmq, Config
 
 experiments = Blueprint('experiments', __name__)
+config = Config()
 
 @experiments.route('/run_experiment')
 def run_experiment():
     session['uid'] = uuid.uuid4()
     return render_template('run_experiment.html')
 
-@experiments.route('/get_ropod_ids', methods=['GET'])
-def get_ropod_ids():
-    msg_data['header']['type'] = "NAME_QUERY"
-    msg_data['payload']['sender_id'] = session['uid'].hex
-    communication_command = "GET_ROPOD_LIST"
-    msg_data_string = json.dumps(msg_data)
-    data = communication_command + "++" + msg_data_string
-
-    ropod_ids = dict()
-    message = ''
+@experiments.route('/get_robot_ids', methods=['GET'])
+def get_robot_ids():
+    robots = list()
+    feedback_msg = ''
     try:
-        query_reply = communicate_zmq(data)
-        if query_reply:
-            ropods = ast.literal_eval(query_reply.decode('ascii'))
-            ropod_ids = []
-            for node in ropods:
-                sender_name = node[0]
-                suffix_idx = sender_name.find('_query_interface')
-                ropod_ids.append(sender_name[0:suffix_idx])
-    # except Exception, exc:
+        robots = config.get_robots()
     except Exception as exc:
-        print('[get_ropod_ids] %s' % str(exc))
-        message = 'Ropod list could not be retrieved'
-    return jsonify(ropod_ids=ropod_ids, message=message)
+        print('[get_robot_ids] %s' % str(exc))
+        feedback_msg = 'An error occurred while retrieving the robot IDs'
+    return jsonify(robot_ids=robots, message=feedback_msg)
 
 @experiments.route('/send_experiment_request', methods=['GET','POST'])
 def send_experiment_request():
     ropod_id = request.args.get('ropod_id', '', type=str)
     experiment = request.args.get('experiment', '', type=str)
 
-    msg_data['header']['type'] = "RUN_EXPERIMENT"
-    msg_data['payload']['sender_id'] = session['uid'].hex
-    msg_data['payload']['ropod_id'] = ropod_id
-    msg_data['payload']['experiment'] = experiment
+    msg_data['header']['type'] = 'ROBOT-EXPERIMENT-REQUEST'
+    msg_data['payload']['userId'] = session['uid'].hex
+    msg_data['payload']['robotId'] = ropod_id
+    msg_data['payload']['experimentType'] = experiment
 
     message = ''
     try:
-        communication_command = "DATA_QUERY"
+        communication_command = "EXPERIMENT"
         msg_data_string = json.dumps(msg_data)
         data = communication_command + "++" + msg_data_string
         _ = communicate_zmq(data)
-    # except Exception, exc:
     except Exception as exc:
         print('[send_experiment_request] %s' % str(exc))
         message = 'Command could not be sent'
