935f7e8b5bdebd552460f0400e34cf3fcca78bd2
Santosh Thoduka <santosh.thoduka@gmail.com>
2018-12-07 17:43:20 +0100

    remove hard-coded map display parameters; move config to database; make functions more reusable by moving to common locations

--- remote_monitoring/blueprints/task_scheduling/task_scheduling.py
+++ remote_monitoring/blueprints/task_scheduling/task_scheduling.py
@@ -1,12 +1,11 @@
 from __future__ import print_function
 import uuid
 import threading
-import math
 
 from flask import Blueprint, jsonify, render_template, request, session
 from flask_socketio import emit
 
-from remote_monitoring.common import msg_data, Config, socketio
+from remote_monitoring.common import msg_data, Config, socketio, MapUtils
 
 status_thread = None
 status_thread_lock = threading.Lock()
@@ -15,6 +14,7 @@ def create_blueprint(communicator):
     task_scheduling = Blueprint('task_scheduling', __name__)
     zyre_communicator = communicator
     config = Config()
+    map_utils = MapUtils()
 
     @task_scheduling.route('/schedule_task')
     def run_experiment():
@@ -58,6 +58,13 @@ def create_blueprint(communicator):
 
     @socketio.on('connect', namespace='/task_scheduling')
     def on_connect():
+        # TODO: eventually this should be based on user selection from the page
+        # for now get current map from the config database
+        current_map = config.get_current_map()
+        map_msg = config.get_map(current_map)
+        map_msg.pop('_id', None)
+        socketio.emit('map', map_msg, namespace='/task_scheduling')
+
         robots = config.get_robots()
         global status_thread
         with status_thread_lock:
@@ -70,20 +77,8 @@ def create_blueprint(communicator):
             robot_pose_msg_list = list()
             for robot in robot_ids:
                 msg = zyre_communicator.get_pose(robot)
-                robot_pose_msg = dict()
                 if msg:
-                    robot_pose_msg['robotId'] = msg['payload']['robotId']
-                    robot_pose_msg['x'] = msg['payload']['pose']['x']
-                    robot_pose_msg['y'] = msg['payload']['pose']['y']
-                    robot_pose_msg['theta'] = msg['payload']['pose']['theta']
-                    t1 = float(msg['payload']['pose']['theta']) + (9*math.pi/10.0)
-                    t2 = float(msg['payload']['pose']['theta']) - (9*math.pi/10.0)
-                    robot_pose_msg['line1x'] = msg['payload']['pose']['x'] + 2*math.cos(t1)
-                    robot_pose_msg['line1y'] = msg['payload']['pose']['y'] + 2*math.sin(t1)
-                    robot_pose_msg['line2x'] = msg['payload']['pose']['x'] + 2*math.cos(t2)
-                    robot_pose_msg['line2y'] = msg['payload']['pose']['y'] + 2*math.sin(t2)
-                    robot_pose_msg['timestamp'] = msg['header']['timestamp']
-                    robot_pose_msg_list.append(robot_pose_msg)
+                    robot_pose_msg_list.append(map_utils.get_robot_pose_msg(msg))
             if robot_pose_msg_list:
                 socketio.emit('robot_pose', robot_pose_msg_list, namespace='/task_scheduling')
             socketio.sleep(1.0)
