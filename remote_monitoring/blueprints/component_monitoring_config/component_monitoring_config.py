e35b0c5b35798d5664494c9916ac713039f93596
Dharmin <dharmingeo@gmail.com>
2019-05-08 17:53:26 +0200

    Corrected the response viewing in front end and removed dead code in backend

--- remote_monitoring/blueprints/component_monitoring_config/component_monitoring_config.py
+++ remote_monitoring/blueprints/component_monitoring_config/component_monitoring_config.py
@@ -23,11 +23,7 @@ def create_blueprint(communicator):
         '''send query request to component_monitoring query interface via ZyreWebCommunicator 
         through pyre message. Send the received response to front end.
         '''
-        feedback_msg = ''
         robot_id = request.args.get('robot_id', '', type=str)
-        print("inside get_config")
-        config = {'a':1, 'b':5}
-        print(config)
         query_msg = {'header':{}, 'payload': {}}
         query_msg['header']['type'] = 'COMP-MON-CONFIG'
         query_msg['header']['timestamp'] = datetime.now().timestamp()
@@ -43,38 +39,7 @@ def create_blueprint(communicator):
                     response=" ", 
                     message="Received no response from component monitoring query interface")
         else:
+            # print(json.dumps(query_result['payload'], indent=2, default=str))
             return jsonify(response=query_result['payload'], message="")
-
-    # @central_operator_console.route(
-    #         '/central_operator_console/send_query_request', methods=['GET', 'POST'])
-    # def send_query_request():
-    #     '''send query request to fms query interface via ZyreWebCommunicator 
-    #     through pyre message
-    #     '''
-    #     query_type = request.args.get('query_id', '', type=str)
-    #     query_type = query_type.upper().replace("_", "-")
-    #     print(query_type)
-        
-    #     robot_id = request.args.get('robot_id', '', type=str)
-    #     task_id = request.args.get('task_id', '', type=str)
-
-    #     query_msg = {'header':{}, 'payload': {}}
-    #     query_msg['header']['type'] = query_type
-    #     query_msg['header']['timestamp'] = datetime.now().timestamp()
-    #     query_msg['header']['metamodel'] = 'ropod-msg-schema.json'
-    #     query_msg['header']['msgId'] =  uuid.uuid4()
-
-    #     query_msg['payload']['senderId'] = str(session['uid'])
-    #     query_msg['payload']['robotId'] = robot_id
-    #     query_msg['payload']['taskId'] = task_id
-    #     print(query_msg)
-
-    #     # query_msg = json.dumps(query_msg, indent=2, default=str)
-    #     query_result = zyre_communicator.get_query_data(query_msg)
-    #     if query_result is None :
-    #         return jsonify(response=" ", message="Received no response from query interface")
-    #     print("received results")
-
-    #     return jsonify(response=query_result['payload'], message="")
 
     return central_operator_console
